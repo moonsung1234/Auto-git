ec872946187748bc76ea080708136b16686b6b5b
moonsung1234 <dit67890@gmail.com>
2021-01-24 21:02:46 +0900

    auto-git

--- launcher.py
+++ launcher.py
@@ -1,39 +1,58 @@
 
 import os
 import time
+import datetime
 import threading
 
 class Setter :
-    def __init__(self, command, before_setter) :
+    def __init__(self, command, setter) :
         self.command = command
-        self.setter = before_setter
+        self.setter = setter
 
 class Launcher :
     def __init__(self) :
         self.thread = None
+        self.date = None
         self.cycle = None
+
+    def __loopSetter(self, setter) :
+        if setter.name == "master" :
+            temp = setter.head_setter.setter
+        
+        else :
+            temp = setter.head_setter
+
+        while temp != None :
+            state = os.system(temp.command)
+
+            if state != 0 :
+                raise Exception("Error state : " + str(state))
+                return
+
+            temp = temp.setter
 
     def __run(self, setter) :
         while True :
-            if setter.name == "master" :
-                temp = setter.head_setter.setter
+            if self.date != None :
+                date_now = datetime.datetime.now()
+
+                if self.date[0] == date_now.year \
+                and self.date[1] == date_now.month \
+                and self.date[2] == date_now.day \
+                and self.date[3] == date_now.hour :
+                    self.__loopSetter(setter)
             
             else :
-                temp = setter.head_setter
+                self.__loopSetter(setter)
 
-            while temp != None :
-                state = os.system(temp.command)
+            if self.cycle != None :
+                time.sleep(self.cycle)
 
-                if state != 0 :
-                    raise Exception("Error state : " + str(state))
-                    return
-
-                temp = temp.setter
-
-            if self.cycle == None :
+            else :
                 break
 
-            time.sleep(self.cycle)
+    def setDate(self, date) :
+        self.date = list(map(int, date.split("/")))
 
     def setCycle(self, cycle) :
         self.cycle = cycle
--- main.py
+++ main.py
@@ -7,5 +7,6 @@ master_commit = master.commit("auto-git")
 master_push = master.push("origin")
 
 launcher = AutoGit.Launcher()
+launcher.setDate("2021/1/24/21")
 launcher.setCycle(5)
 launcher.run(master)
